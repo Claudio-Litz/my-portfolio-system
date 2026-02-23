5315565e9fd4c793ca4f56d25503f1a08fcdc354
Claudio Gabriel Litz <claudioglitz1@gmail.com>
2026-02-23 23:08:16 +0000

    Test of link to linkedIn and github 4

--- system/src/components/About.tsx
+++ system/src/components/About.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import { User } from 'lucide-react';
+import Image from 'next/image';
 import { useLanguage } from '@/context/LanguageContext';
 
 const About = () => {
